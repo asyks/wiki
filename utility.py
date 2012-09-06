c8421a44456987fadc894b419168a564da7ff315
aaron <aaron@minityke.localdomain>
2012-09-06 19:27:41 -0400

    forgot to add my utility.py file

---
+++ utility.py
@@ -0,0 +1,68 @@
+import hashlib
+import hmac
+import string
+import random
+import re
+
+from datetime import datetime
+
+# date format and string substitution procedures for wiki pages last edited footer
+
+def format_datetime(date_time):
+  time_format = '%d, %h %Y %H:%M:%S' 
+  return date_time.strftime(time_format)
+
+def make_last_edit_str(time):
+  return 'This page was last edited on: %s' % time
+ 
+# sign-up form validation stuff
+
+USER_RE = re.compile("^[a-zA-Z0-9_-]{3,20}$")
+PASS_RE = re.compile("^.{3,20}$")
+EMAIL_RE = re.compile("^[\S]+@[\S]+\.[\S]+$")
+
+def user_validate(u):
+  if USER_RE.match(u):
+    return True
+
+def password_validate(p):
+  if PASS_RE.match(p):
+    return True
+
+def password_verify(p,v):
+  if p == v:
+    return True
+
+def email_validate(e): 
+  if not e:
+    return True
+  if e and EMAIL_RE.match(e):
+    return True
+
+# cookie setting stuff
+
+secret = 'you will never guess me'
+
+def make_secure_val(val):
+  return '%s|%s' % (val, hmac.new(secret, val).hexdigest())
+
+def check_secure_val(secure_val):
+  val = secure_val.split('|')[0]
+  if secure_val == make_secure_val(val):
+    return val 
+
+# password hashing stuff
+
+def make_salt():
+  return ''.join(random.choice(string.letters) for x in range(5))
+
+def make_hash(un, pw, salt=None):
+  if not salt:
+    salt = make_salt()
+  h = hashlib.sha256(un + pw + salt).hexdigest()
+  return '%s|%s' % (salt, h)
+
+def check_hash(un, pw, h):
+  salt = h.split('|')[0]
+  if h == make_hash(un, pw, salt):
+    return True
